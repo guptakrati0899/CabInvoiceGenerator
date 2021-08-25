7d7159412109412f876dd1387874db5af4e491f3
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 12:48:50 +0530

    Added to calculate fair of multiple rides

--- main/java/cabinvoice/InvoiceService.java
+++ main/java/cabinvoice/InvoiceService.java
@@ -1,16 +1,21 @@
 package cabinvoice;
+
 public class InvoiceService {
 
-    private static final double MINFARE = 5;
-    private static final double MINCOSTPERKM = 10;
-    private static final int COSTPERTIME = 1;
+        private static final double MINFARE = 5;
+        private static final double MINCOSTPERKM = 10;
+        private static final int COSTPERTIME = 1;
 
-    public double calculateFare(double distance, int time) {
-        double totalFare = distance * MINCOSTPERKM + time * COSTPERTIME;
-        if(totalFare < MINFARE) {
-            return MINFARE;
+        public double calculateFare(double distance, int time) {
+            double totalFare = distance * MINCOSTPERKM + time * COSTPERTIME;
+            return Math.max(totalFare, MINFARE);
         }
-        return totalFare;
-    }
-}
 
+        public double calculateFare(Ride[] rides) {
+            double totalFare = 0;
+            for (Ride ride:rides) {
+                totalFare += this.calculateFare(ride.distance, ride.time);
+            }
+            return totalFare;
+        }
+    }
