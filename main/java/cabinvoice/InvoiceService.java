5297b092238dbea47c02dbe58ccc6f1ce1877150
KratiGupta <kratigupta8545@gmail.com>
2021-08-25 12:34:10 +0530

    Added Welcome message successfully

---
+++ main/java/cabinvoice/InvoiceService.java
@@ -0,0 +1,8 @@
+package cabinvoice;
+
+public class InvoiceService {
+        public static void main(String[] args) {
+            System.out.println("Welcome to the Cab Invoice Generator");
+        }
+    }
+
